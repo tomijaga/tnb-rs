479061adfea4ece45217e197cf77cbb13fecf500
Tomi Jaga <jagatomi12@gmail.com>
2021-11-25 16:56:31 -0500

    update docs

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["tnb_rs"] = {"name":"","files":["account.rs","hd_wallet.rs","lib.rs"]};
+sourcesIndex["tnb_rs"] = {"name":"","dirs":[{"name":"models","files":["block.rs","mod.rs","search_params.rs","signed_message.rs","transaction.rs"]},{"name":"nodes","files":["confirmation_validator.rs","mod.rs","primary_validator.rs","regular_node.rs","server_node.rs","traits.rs","validator.rs"]},{"name":"responses","files":["common.rs","mod.rs","validator.rs"]}],"files":["account.rs","client.rs","hd_wallet.rs","lib.rs","node_client.rs","utils.rs","wallet.rs"]};
 createSourceSidebar();
